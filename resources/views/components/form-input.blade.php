807cb7b0d26c0c254aeb6e3427ff778488f016c2
cnedosto <c.nedosto@gmail.com>
2024-03-01 18:14:05 +0100

    refacto(inputs): create input component with livewire

---
+++ resources/views/components/form-input.blade.php
@@ -0,0 +1,34 @@
+@props([
+'type' => "text",
+'label' => "",
+'required' => "false",
+])
+
+<div>
+    <label for="{{$attributes->whereStartsWith('wire:model')->first()}}" class="block text-sm font-medium leading-6 text-gray-900">{{$label}}</label>
+    <div class="relative mt-2 rounded-md shadow-sm">
+        <input {{$attributes->whereStartsWith('wire:model')}} 
+        id="{{$attributes->whereStartsWith('wire:model')->first()}}" 
+        name="{{$attributes->whereStartsWith('wire:model')->first()}}" 
+        type="{{$type}}" 
+        required="{{$required}}" 
+        @error($attributes->whereStartsWith('wire:model')->first())
+        class="block w-full rounded-md border-0 py-1.5 pr-10 text-red-900 ring-1 ring-inset ring-red-300 placeholder:text-red-300 focus:ring-2 focus:ring-inset focus:ring-red-500 sm:text-sm sm:leading-6"
+        aria-invalid="true" 
+        aria-describedby="firstName-error"
+        @else
+        class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+        @endif
+        >
+        @error($attributes->whereStartsWith('wire:model')->first())
+        <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-3">
+            <svg class="h-5 w-5 text-red-500" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                <path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-5a.75.75 0 01.75.75v4.5a.75.75 0 01-1.5 0v-4.5A.75.75 0 0110 5zm0 10a1 1 0 100-2 1 1 0 000 2z" clip-rule="evenodd" />
+            </svg>
+        </div>
+        @enderror
+    </div>
+    @error($attributes->whereStartsWith('wire:model')->first())
+    <p class="mt-2 text-sm text-red-600" id="email-error">Not a valid <span class="lowercase">{{$label}}</span>.</p>
+    @enderror
+</div>
--- resources/views/livewire/auth/register.blade.php
+++ resources/views/livewire/auth/register.blade.php
@@ -15,47 +15,23 @@
                     <div>
                         <form wire:submit.prevent="register" class="space-y-6">
                             <div class="flex gap-x-4">
-                                <div>
-                                    <label for="firstName" class="block text-sm font-medium leading-6 text-gray-900">First name</label>
-                                    <div class="mt-2">
-                                        <input wire:model="firstName" id="firstName" name="firstName" type="text" required class="block w-full rounded-md border-0 py-1.5 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
-                                    </div>
-                                </div>
-                                <div>
-                                    <label for="lastName" class="block text-sm font-medium leading-6 text-gray-900">Last name</label>
-                                    <div class="mt-2">
-                                        <input wire:model="lastName" id="lastName" name="lastName" type="text" required class="block w-full rounded-md border-0 py-1.5 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
-                                    </div>
-                                </div>
+                                <x-form-input wire:model="firstName" type="text" inputName="firstName" label="First name" :required="true" />
+                                <x-form-input wire:model="lastName" type="text" inputName="lastName" label="Last name" :required="true" />
                             </div>
                             <div>
-                                <label for="companyName" class="block text-sm font-medium leading-6 text-gray-900">Company name</label>
-                                <div class="mt-2">
-                                    <input wire:model="companyName" id="companyName" name="companyName" type="text" required class="block w-full rounded-md border-0 py-1.5 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
-                                </div>
+                                <x-form-input wire:model="companyName" type="text" inputName="companyName" label="Company name" :required="true" />
                             </div>
                             <div>
-                                <label for="email" class="block text-sm font-medium leading-6 text-gray-900">Email address</label>
-                                <div class="mt-2">
-                                    <input wire:model="email" id="email" name="email" type="email" autocomplete="email" required class="block w-full rounded-md border-0 py-1.5 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
-                                </div>
+                                <x-form-input wire:model="email" type="email" inputName="email" label="Email address" :required="true" />
                             </div>
 
                             <div>
-                                <label for="password" class="block text-sm font-medium leading-6 text-gray-900">Password</label>
-                                <div class="mt-2">
-                                    <input wire:model="password" id="password" name="password" type="password" autocomplete="current-password" required class="block w-full rounded-md border-0 py-1.5 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
-                                </div>
+                                <x-form-input wire:model="password" type="password" inputName="password" label="Password" :required="true" />
                             </div>
 
                             <div>
-                                <label for="passwordConfirmation" class="block text-sm font-medium leading-6 text-gray-900">Password confirmation</label>
-                                <div class="mt-2">
-                                    <input wire:model="passwordConfirmation" id="passwordConfirmation" name="passwordConfirmation" type="password" autocomplete="current-password" required class="block w-full rounded-md border-0 py-1.5 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
-                                </div>
+                                <x-form-input wire:model="passwordConfirmation" type="password" inputName="passwordConfirmation" label="Password confirmation" :required="true" />
                             </div>
-
-
                             <div>
                                 <button type="submit" class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Register</button>
                             </div>

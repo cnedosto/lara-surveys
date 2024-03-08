a5ee953b29daaf428c84815a23e4c557bbc8f93d
cnedosto <c.nedosto@gmail.com>
2024-03-08 13:20:51 +0100

    feat(surveys): adding participants count & survey status

--- app/Models/Survey.php
+++ app/Models/Survey.php
@@ -15,6 +15,24 @@ class Survey extends Model
         return $this->hasMany(SurveyQuestion::class);
     }
 
+    public function participants()
+    {
+        return $this->questions()
+            ->join('user_answers', 'survey_questions.id', '=', 'user_answers.question_id')
+            ->select('user_answers.user_id')
+            ->distinct()
+            ->count('user_answers.user_id');
+    }
+
+    public function status()
+    {
+        $totalUsers = User::where('tenant_id', $this->tenant_id)->count();
+
+        $answeredUsers = $this->participants();
+
+        return $answeredUsers >= $totalUsers ? 'answered' : 'ongoing';
+    }
+
     protected static function booted(): void
     {
         static::addGlobalScope(new TenantScope);
--- resources/views/livewire/survey-list.blade.php
+++ resources/views/livewire/survey-list.blade.php
@@ -3,8 +3,12 @@
         <tr>
             <td class="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-white sm:pl-0">{{ $survey->name }}</td>
             <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-300">{{ $survey->questions_count }}</td>
-            <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-300"></td>
-            <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-300"></td>
+            <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-300">{{ $survey->participants() }}</td>
+            <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-300">
+                <span class="capitalize inline-flex items-center rounded-md px-2 py-1 text-xs font-medium ring-inset ring-1
+                {{$survey->status() == 'ongoing' ? 'bg-indigo-400/10 text-indigo-400 ring-indigo-400/30' : 'bg-green-500/10 text-green-400 ring-green-500/20'}}">
+                {{ $survey->status() }}</span>
+            </td>
             <td class="relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-0 flex flex-col gap-y-2">
                 <div @click="$dispatch('openSurvey', { surveyId: {{$survey->id}} }); openAnswerSurveyModal = true" class="text-indigo-400 hover:text-indigo-300 cursor-pointer">Open</div>
             @if(auth()->user() && auth()->user()->role === 'admin')
